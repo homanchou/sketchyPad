9facb4c19bb89a3b7d1434be5c2340da6e03026f
Homan Chou <homanchou@Homan-Chous-MacBook-Pro-2.local>
2011-08-24 15:24:41 -0700

    refactor to use hash for public functions

--- jsSketchyPad.js
+++ jsSketchyPad.js
@@ -34,40 +34,51 @@
     }; //end fn.sketchyPad function
 
 
-    //static functions
-    $.sketchyPad = {};
+    //overridable static functions 
+  $.sketchyPad = {
     
-     //default settings
-    $.sketchyPad.defaults = {
-           width: 600,
-           height: 600,
-           styleSheetPath: 'sketchyPad.css',
-           brushSize: 1,
-           opacity: 0.9,
-           color: [0,0,0],
-           defaultBrushType: "simple"
-    };
+    //default settings
+    defaults : {
+         width: 600,
+         height: 600,
+         styleSheetPath: 'sketchyPad.css',
+         brushSize: 1,
+         opacity: 0.9,
+         color: [0,0,0],
+         defaultBrushType: "simple"
+    },
 
-    $.sketchyPad.opts = {};
+    //options
+    opts: {},
 
-
-    $.sketchyPad.injectCSS = function() {
-          $('head').append('<link rel="stylesheet" href="' + $.sketchyPad.opts.styleSheetPath + '" type="text/css" />');
-    };
+    injectCSS: function() {
+        $('head').append('<link rel="stylesheet" href="' + $.sketchyPad.opts.styleSheetPath + '" type="text/css" />');
+    },
     
-    $.sketchyPad.createCanvas = function() {
+    createCanvas: function() {
        element.append("<canvas id='top' class='sketch_layer' width='"+$.sketchyPad.opts.width+"px' height='"+$.sketchyPad.opts.height+"'></canvas>")
        top_canvas = $('#top');
        //get the offset in case the window is resized, coordinates are always relative to canvas 
        offset = top_canvas.offset();
-    };
+    },
     
-    $.sketchyPad.setBrush = function(brushType) {
+    setBrush: function(brushType) {
         var context = top_canvas.get(0).getContext("2d");
         brush = eval("new " + brushType + "(context)");    
-    };
+    },
 
-    $.sketchyPad.registerEvents = function() {
+    setColor: function(stringOrArray) {
+        
+    },
+
+    setBrushSize: function(brushSize) {
+      console.log(this);
+    },
+
+    setOpacity: function(opacity) {
+    },
+
+    registerEvents: function() {
 
 
          $(window).resize(function() { offset = top_canvas.offset();  });
@@ -76,38 +87,38 @@
          $(window).bind('mousemove', $.sketchyPad.onWindowMouseMove);
          //canvas detect mouse down -- register more events
          top_canvas.bind('mousedown', $.sketchyPad.onCanvasMouseDown);
-    };
+    },
     
-    $.sketchyPad.onWindowMouseMove = function(event) {
+    onWindowMouseMove: function(event) {
          lastPoint = currentPoint;
          currentPoint = $.sketchyPad.getBrushPoint(event);
         
-    };
+    },
     
-    $.sketchyPad.getBrushPoint = function(event) {
+    getBrushPoint: function(event) {
        return {x:event.clientX - offset.left + window.pageXOffset, y:event.clientY - offset.top + window.pageYOffset}
-    };
+    },
 
-    $.sketchyPad.onCanvasMouseDown = function(event) {
+    onCanvasMouseDown: function(event) {
       
       //additional handlers bound at window level, that way if pen exits canvas border, we can still receive events
       $(window).bind('mouseup', $.sketchyPad.onWindowMouseUp);
       $(window).bind('mousemove', $.sketchyPad.onCanvasMouseMove);
     
       brush.strokeStart(currentPoint);
-    };
+    },
 
-    $.sketchyPad.onWindowMouseUp = function(event) {
+    onWindowMouseUp: function(event) {
       brush.strokeEnd();
       $(window).unbind('mouseup', $.sketchyPad.onWindowMouseUp);
       $(window).unbind('mousemove', $.sketchyPad.onCanvasMouseMove);
-    };
+    },
     
-    $.sketchyPad.onCanvasMouseMove = function(event) {
+    onCanvasMouseMove: function(event) {
       brush.stroke(currentPoint);
-    };
+    }
       
-
+    }; //end overridable functions
    
 // end of closure
 

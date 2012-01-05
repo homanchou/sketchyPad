d8f9671fcd243a8ddcf8aac62e89917318177780
Homan Chou <homanchou@gmail.com>
2012-01-05 14:07:32 -0800

    fix simple brush color stuck bug

--- brushes/simple.js
+++ brushes/simple.js
@@ -12,9 +12,6 @@ Simple.prototype = {
     this.context = $('#layer1').get(0).getContext("2d");
     this.context.globalCompositeOperation = 'source-over';
     this.context.lineCap = 'round';
-    this.context.lineWidth = sketchyPad.opts.brushSize;
-    this.context.strokeStyle = sketchyPad.getRGBA();
-    this.context.fillStyle = sketchyPad.getRGBA();
   },
   strokeStart: function(sketchyPad) { 
     this.prevPoint = sketchyPad.opts.currentPoint;
@@ -22,7 +19,9 @@ Simple.prototype = {
   stroke: function(sketchyPad) {
     this.travel = true;
     var point = sketchyPad.opts.currentPoint;
-    
+    this.context.lineWidth = sketchyPad.opts.brushSize;
+    this.context.strokeStyle = sketchyPad.getRGBA();
+    this.context.fillStyle = sketchyPad.getRGBA();
     this.context.beginPath();
     this.context.moveTo(this.prevPoint.x, this.prevPoint.y);
     this.context.lineTo(point.x, point.y);
@@ -30,15 +29,17 @@ Simple.prototype = {
     this.prevPoint = point;
   },
   strokeEnd: function(sketchyPad) {
-      //put an extra one in case they are making dots without moving 
-      var new_point = sketchyPad.opts.currentPoint;
-      if (new_point) {
-          
+    //put an extra one in case they are making dots without moving 
+    var new_point = sketchyPad.opts.currentPoint;
+    if (new_point) {
+        this.context.lineWidth = sketchyPad.opts.brushSize;
+        this.context.strokeStyle = sketchyPad.getRGBA();
+        this.context.fillStyle = sketchyPad.getRGBA();
         this.context.beginPath();
         this.context.arc(new_point.x, new_point.y,parseInt(this.context.lineWidth)/2, 0, Math.PI * 2, true);
         this.context.closePath();
         this.context.fill();
-      }
+    }
     
   }
 };
